1d27f345817fc63c92a486c73736e890cba416de
lwilhem <wilhem.lecanu@edu.devinci.fr>
2025-07-31 09:07:32 +0200

    chore: listed all eslint js rules (todo: triage the various rulesets)

--- eslint.config.js
+++ eslint.config.js
@@ -3,4 +3,8 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   type: 'lib',
   pnpm: true,
+
+  typescript: {
+    tsconfigPath: './tsconfig.json',
+  },
 })
---
+++ scripts/test.ts
@@ -0,0 +1,5 @@
+import js from '@eslint/js'
+
+const rules = js.configs.recommended.rules
+
+console.log(JSON.stringify(rules, null, 2))
