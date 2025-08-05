f1e9309ebcd90a9b562cfd0229566e9f290047ed
lwilhem <wilhem.lecanu@edu.devinci.fr>
2025-08-05 09:50:55 +0200

    chore: updated deps && js rules cleanup

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,4 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   type: 'lib',
   pnpm: true,
-
-  typescript: {
-    tsconfigPath: './tsconfig.json',
-  },
 })
--- src/utils.ts
+++ src/utils.ts
@@ -70,3 +70,100 @@ export async function combine(...configs: Awaitable<Config | Config[]>[]): Promi
   const resolved = await Promise.all(configs)
   return resolved.flat()
 }
+
+export const parserPlain = {
+  meta: {
+    name: 'parser-plain',
+  },
+  parseForESLint: (code: string) => ({
+    ast: {
+      body: [],
+      comments: [],
+      loc: { end: code.length, start: 0 },
+      range: [0, code.length],
+      tokens: [],
+      type: 'Program',
+    },
+    scopeManager: null,
+    services: { isPlain: true },
+    visitorKeys: {
+      Program: [],
+    },
+  }),
+}
+
+/**
+ * Rename plugin prefixes in a rule object.
+ * Accepts a map of prefixes to rename.
+ *
+ * @example
+ * ```ts
+ * import { renameRules } from '@antfu/eslint-config'
+ *
+ * export default [{
+ *   rules: renameRules(
+ *     {
+ *       '@typescript-eslint/indent': 'error'
+ *     },
+ *     { '@typescript-eslint': 'ts' }
+ *   )
+ * }]
+ * ```
+ */
+export function renameRules(
+  rules: Record<string, any>,
+  map: Record<string, string>,
+): Record<string, any> {
+  return Object.fromEntries(
+    Object.entries(rules)
+      .map(([key, value]) => {
+        for (const [from, to] of Object.entries(map)) {
+          if (key.startsWith(`${from}/`))
+            return [to + key.slice(from.length), value]
+        }
+        return [key, value]
+      }),
+  )
+}
+
+/**
+ * Rename plugin names a flat configs array
+ *
+ * @example
+ * ```ts
+ * import { renamePluginInConfigs } from '@antfu/eslint-config'
+ * import someConfigs from './some-configs'
+ *
+ * export default renamePluginInConfigs(someConfigs, {
+ *   '@typescript-eslint': 'ts',
+ *   '@stylistic': 'style',
+ * })
+ * ```
+ */
+export function renamePluginInConfigs(configs: Config[], map: Record<string, string>): Config[] {
+  return configs.map((i) => {
+    const clone = { ...i }
+    if (clone.rules)
+      clone.rules = renameRules(clone.rules, map)
+    if (clone.plugins) {
+      clone.plugins = Object.fromEntries(
+        Object.entries(clone.plugins)
+          .map(([key, value]) => {
+            if (key in map)
+              return [map[key], value]
+            return [key, value]
+          }),
+      )
+    }
+    return clone
+  })
+}
+
+export function toArray<T>(value: T | T[]): T[] {
+  return Array.isArray(value) ? value : [value]
+}
+
+export async function interopDefault<T>(m: Awaitable<T>): Promise<T extends { default: infer U } ? U : T> {
+  const resolved = await m
+  return (resolved as any).default || resolved
+}
